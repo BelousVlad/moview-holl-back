f6ebdf7e0d24c3257f8e48a41223a815533bad0e
Vlad Belous <chebureck333@gmail.com>
2021-07-21 16:26:23 +0300

    categories n->n relation

--- app/Database/Migrations/2021-07-21-111446_Category.php
+++ app/Database/Migrations/2021-07-21-111446_Category.php
@@ -22,17 +22,38 @@ class Category extends Migration
 
 		$this->forge->createTable('categories');
 		
-		$this->forge->addColumn('movies', [
-			'category_id' => [
+		// $this->forge->addColumn('movies', [
+		// 	'category_id' => [
+		// 		'type' => 'INT'
+		// 	],
+		// 	'CONSTRAINT category_id_fk FOREIGN KEY(`category_id`) REFERENCES `categories`(`category_id`)'
+		// ]);
+
+		$this->forge->addField([
+			'movie_category_id' => [
+				'type' => 'INT',
+				'auto_increment' => TRUE
+			],
+			'movie_id' => [
 				'type' => 'INT'
 			],
-			'CONSTRAINT category_id_fk FOREIGN KEY(`category_id`) REFERENCES `categories`(`category_id`)'
+			'category_id' => [
+				'type' => 'INT'
+			]
 		]);
+
+		$this->forge->addPrimaryKey('movie_category_id');
+
+		$this->forge->addForeignKey('movie_id', 'movies', 'movie_id');
+		$this->forge->addForeignKey('category_id', 'categories', 'category_id');
+
+		$this->forge->createTable('movie_category');
 	}
 
 	public function down()
 	{
-		$this->forge->dropColumn('movies', 'category_id');
+		// $this->forge->dropColumn('movies', 'category_id');
+		$this->forge->dropTable('movie_category');
 		$this->forge->dropTable('categories');
 	}
 }
